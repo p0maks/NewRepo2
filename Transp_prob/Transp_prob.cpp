ad929112933bf760137dd083f35145eb17b1d541
p0maks <golosova_v19@mail.ru>
2022-07-13 20:27:29 +0300

    up commit

--- Transp_prob/Transp_prob.cpp
+++ Transp_prob/Transp_prob.cpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <iomanip>
 #include <Windows.h>
-
 using namespace std;
 
 const int N = 6;
@@ -22,4 +21,42 @@ void main(void)
 {
     setlocale(LC_ALL, "rus");
 
+    int A[M] = {
+    500,
+    400,
+    1000
+    },
+        C[M][N] = {
+            { 100, 80, 40,   3, 50,  56 },
+            { 20,   9, 42,   8, 15,   7 },
+            { 80,   1,  1, 100,  2,  10 }
+    }, i, j, X[3] = { 0, 0, 0 };
+    X[0] = C[0][0];
+    for (i = 0, j = 0; j < N; ++i)
+    {
+        if (i == M)
+        {
+            cz[X[1]].dat[X[2]] = X[0];
+            i = 0;
+            ++j;
+            X[0] = C[i][0];
+            X[1] = 0;
+        }
+        if ((C[i][j] * A[i] < X[0] * A[X[1]]))
+        {
+            X[0] = C[i][j];
+            X[1] = i;
+            X[2] = j;
+        }
+    }
+    for (i = 0; i < M; ++i)
+    {
+        cout << "С фабрики " << cz[i].FactN
+            << " выгодно доставлять груз до населенного пункта № ";
+        for (j = 0; j < N; ++j)
+            if (cz[i].dat[j] > -1) cout << j + 1 << " ";
+        cout << "\n";
+    }
+    system("pause");
+    return;
 }
